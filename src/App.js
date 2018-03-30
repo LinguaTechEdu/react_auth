d79ab6a260d86e4dac771de36f3a83840057be79
Dev <dev@divethree.com>
2018-03-29 19:29:34 -0700

    Basic.

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+import {
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom';
+
+import About from './components/About';
+
+class App extends Component {
+    render() {
+        return (
+            <header>
+               <h1>Learn Routing</h1>
+               <nav>
+                   {/* Create our nav bar links using the Link element from react router */}
+                   <ul>
+                       <li><Link to="/about">About</Link></li>
+                   </ul>
+               </nav>
+
+                {/* Create the routes. This will not appear on the page. */}
+               <div className="main">
+                   <Switch>
+                       <Route path="/about" component={ About } />
+                   </Switch>
+               </div>
+           </header>
+        );
+    }
+}
+
+export default App;
